ff09e3cd8acfd54a36b4df7162fe2a78479b0d11
lotemh <lotemhiki@gmail.com>
2016-01-23 18:15:34 +0200

    add setup files

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by lotem on 1/23/2016.
+ */
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+    response.render('index');
+});
+
+app.get('/pshat', function(request, response) {
+    response.json(pshat.json);
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+    console.log(process.env.MOTO);
+});
