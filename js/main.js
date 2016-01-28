e5a8b4c3cba44d32478bb57fff9b3d23aae80af7
lotemh <lotemh@wix.com>
2016-01-28 18:46:20 +0200

    adding reac and redux

--- js/main.js
+++ js/main.js
@@ -1,57 +1,107 @@
 /**
  * Created by lotem on 1/23/2016.
  */
+var index;
 
-var key = "";
-var keys = [];
-var shown = [];
+function reducer(state, action){
+    var item;
 
-function showDefinition(){
-    var definitionElement = getDefinitionLabel();
-    definitionElement.innerHTML = data[key];
-    showButton(definitionElement);
-    hideButton(getDefinitionButton());
-}
-
-function hideButton(button){
-    button.setAttribute("class", 'hidden');
-}
-
-function showButton(element){
-    element.setAttribute("class", 'definition');
-}
-
-function getDefinitionButton(){
-    return document.querySelector("#defineContainer button");
-}
-
-function getDefinitionLabel(){
-    return document.querySelector("#definition");
-}
-
-function getDefinitionKeyElement(){
-    return document.querySelector("#musag");
-}
-
-function nextDefinition(){
-    getDefinitionKeyElement().innerHTML = getDefinitionKey();
-    showButton(getDefinitionButton());
-    hideButton(getDefinitionLabel());
-}
-
-function getDefinitionKey(){
-    if (keys.length === 0){
-        keys = shown;
-        shown = [];
+    if (typeof state === 'undefined'){
+        index = 0;
+        item = data[index];
+        return {
+            key: item.key,
+            data: item.value,
+            definition: 'button'
+        };
     }
-    key = keys.pop();
-    shown.push(key);
-    return key;
+    switch (action.type){
+        case 'SHOW_DEFINITION':
+            return  _.defaults({definition: 'label'}, state);
+        case 'NEXT':
+            index = increaseIndex(index);
+            item = data[index];
+            return {
+                key: item.key,
+                data: item.value,
+                definition: 'button'
+            };
+        default:
+            return state;
+    }
 }
 
-window.onload = function(){
-    for (var k in data){
-        keys.push(k);
+function increaseIndex(index){
+    return (index + 1) % data.length;
+}
+
+var store = Redux.createStore(reducer);
+store.subscribe(render);
+
+var Definition = React.createClass({
+    showDefinition: function(){
+        store.dispatch({type: "SHOW_DEFINITION"});
+    },
+    nextDefinition: function(){
+        store.dispatch({type: "NEXT"});
+    },
+    render: function(){
+        return (
+            <div>
+                <div id="defineContainer" className={store.getState().definition}>
+                    <span>
+                        <button className="definition" onClick={this.showDefinition}> Show Definition</button>
+                    </span>
+
+                    <DataLabel data={store.getState().data}/>
+                    <Musag data={store.getState().key}/>
+                </div>
+                <div>
+                    <button className="btn" onClick={this.nextDefinition}>Next</button>
+                </div>
+            </div>
+        );
     }
-    nextDefinition();
-};
+});
+
+var Musag = React.createClass({
+    render: function() {
+        return (
+            <span>{this.props.data}</span>
+        );
+    }
+});
+
+var DataLabel = React.createClass({
+    render: function() {
+        return (
+            <label>{this.props.data}</label>
+        );
+    }
+});
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div className="App">
+                <div className="menu">
+                    <h1>Test is Coming</h1>
+                </div>
+
+                <div className="definitionLabel">Define the next term:</div>
+                <div className="musagContainer">
+                    <Definition data={data}/>
+                </div>
+            </div>
+        );
+    }
+});
+
+function render(){
+    ReactDOM.render(
+        <App />,
+        document.getElementById("root")
+    );
+}
+
+render();
